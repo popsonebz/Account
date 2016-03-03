f28c7e7ce87ed3c2a1a2f88bb54585e7021e38de
popoola <popoolaebenezer@gmail.com>
2016-03-03 13:17:12 +0200

    Using set and get method for validation

---
+++ src/Account.java
@@ -0,0 +1,14 @@
+/**
+ * Created by popoola on 2016/03/03.
+ */
+public class Account {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/AccoutTest.java
@@ -0,0 +1,20 @@
+/**
+ * Created by popoola on 2016/03/03.
+ */
+import java.util.Scanner;
+public class AccoutTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Account myAccount = new Account();
+
+        System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+
+        System.out.println("Please enter the name: ");
+        String theName = input.nextLine();
+        myAccount.setName(theName);
+        System.out.println();
+
+        System.out.printf("Name in object myAccount is:%n%s%n", myAccount.getName());
+    }
+}
